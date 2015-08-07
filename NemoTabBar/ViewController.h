1d79566586829765b45f218ece92dd1de19852a1
LiuDongQi <liudongqi@LiuDongQideMacBook-Pro.local>
2015-08-07 12:34:33 +0800

    Initial Commit

---
+++ NemoTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NemoTabBar
+//
+//  Created by LiuDongQi on 15/8/7.
+//  Copyright (c) 2015年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NemoTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NemoTabBar
+//
+//  Created by LiuDongQi on 15/8/7.
+//  Copyright (c) 2015年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
